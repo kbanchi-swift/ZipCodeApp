750475de603e17017f0ec03cd71dbead82412966
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-06 11:16:59 +0900

    Initial Commit

---
+++ ZipCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZipCodeApp
+//
+//  Created by 伴地慶介 on 2021/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
